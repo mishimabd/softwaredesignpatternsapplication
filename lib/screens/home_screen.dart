c74b2ce31673a4417ca972b3d21e1dce36e60d2e
mishimabd <meirkhanabdullayev@yahoo.com>
2022-10-24 23:03:19 +0600

    gap

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -38,7 +38,8 @@ class _HomeScreenState extends State<HomeScreen> {
                   TextSpan(
                       text: '\nProject for finishing all 5 patterns!',
                       style: TextStyle(
-                          fontSize: 12,))
+                        fontSize: 12,
+                      ))
                 ])),
             SizedBox(height: MediaQuery.of(context).size.height / 2.5),
             InkWell(
